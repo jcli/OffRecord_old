73dccb253cfd3c38d40cd8b29037e4ec4bbc71f8
jcli <jcli@netflix.com>
2016-04-18 14:01:36 -0700

    added tabs

--- app/src/main/java/com/sinova/jcli/offrecord/MainActivity.java
+++ app/src/main/java/com/sinova/jcli/offrecord/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,10 +16,46 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     public static String TAG = MainActivity.class.getSimpleName();
     public static GoogleDriveModel mGDriveModel;
+
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+
+    private class OffRecordPagerAdapter extends FragmentPagerAdapter{
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public OffRecordPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +82,18 @@ public class MainActivity extends AppCompatActivity {
             JCLog.log(JCLog.LogLevel.WARNING, JCLog.LogAreas.UI, "Fresh app start.  Creating GoogleDriveModel.");
             mGDriveModel = new GoogleDriveModel(this);
         }
+
+        // setup tabs
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        OffRecordPagerAdapter adapter = new OffRecordPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MainActivityFragment(), "ONE");
+        // add more tab fragments
+        adapter.addFragment(new MainActivityFragment(), "tests");
+        mViewPager.setAdapter(adapter);
+
+        mTabLayout = (TabLayout) findViewById(R.id.tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+
         JCLog.log(JCLog.LogLevel.INFO, JCLog.LogAreas.UI, "onCreated called.");
     }
 
